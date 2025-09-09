154babd9dba8c7c34d84100044a83b1fdac409eb
Loris Jared Ndonga <130836683+ndongaloris@users.noreply.github.com>
2025-09-09 19:27:24 +0000

    Tabs done

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -1,15 +1,42 @@
-import { StyleSheet} from 'react-native'
+import { StyleSheet } from 'react-native'
 import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons';
+
 
 const DashboardLayout = () => {
     return (
         <Tabs screenOptions={{
             headerShown: false,
             tabBarActiveTintColor: "white",
-            tabBarStyle:{
+            tabBarStyle: {
                 backgroundColor: "#353F54",
             }
-        }} />
+        }}>
+            <Tabs.Screen name='home' options={{
+                title: "Home",
+                tabBarIcon: () => (
+                    <Ionicons name="home" size={30} color="white" />
+
+                )
+            }} />
+            <Tabs.Screen name='add' options={{ title: "Add",
+                tabBarIcon: () => (
+                    <Ionicons name="add-circle-sharp" size={30} color="white" />
+
+                )
+             }} />
+            <Tabs.Screen name='profile' options={{ title: "Profile",
+                tabBarIcon: () => (
+                    <Ionicons name="person" size={30} color="white" />
+
+                )
+             }} />
+            <Tabs.Screen name='settings' options={{ title: "Settings",
+                tabBarIcon: () => (
+                    <Ionicons name="settings" size={30} color="white" />
+                )
+             }} />
+        </Tabs>
     )
 }
 
