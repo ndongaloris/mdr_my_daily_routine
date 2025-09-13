a99b528734a85cdda78d3f4326abaf2e1d37b4ea
Loris Jared Ndonga <130836683+ndongaloris@users.noreply.github.com>
2025-09-13 09:57:31 +0000

    profile layout completed

--- app/(dashboard)/profile.jsx
+++ app/(dashboard)/profile.jsx
@@ -1,29 +1,62 @@
 import { StyleSheet, Text, View } from 'react-native';
 import ThemedView from '../../components/ThemedView';
 import { Ionicons } from '@expo/vector-icons';
+import { withLayoutContext } from 'expo-router';
 
 
 const profile = () => {
   return (
     <ThemedView>
-      <Text>profile</Text>
       <View style={styles.profileContainer}>
         <Ionicons name="person-circle" size={80} color="#fff" />
         <Text style={styles.profileName}>Loris J. Ndonga</Text>
       </View>
 
-      <View>
-        <Text style={styles.profileName}>Appearance</Text>
-        <Text style={styles.profileName}>Dark Mode
-        </Text>
-        <Text style={styles.profileName}>Switch to dark theme</Text>
+      <View style={styles.report}>
+        <View style={styles.reportCard}>
+          <Ionicons style={{ textAlign: "center", }} name="calendar" size={30} color="#fff" />
+          <Text style={styles.title}>Today's Task</Text>
+          <Text style={styles.title}>5</Text>
+        </View>
+        <View style={styles.reportCard}>
+          <Ionicons style={{ textAlign: "center", }} name="radio-button-on" size={30} color="#fff" />
+          <Text style={styles.title}>Streak</Text>
+          <Text style={styles.title}>12 Days</Text>
+        </View>
       </View>
-      <Text style={styles.profileName}>Preference</Text>
-      <Text style={styles.profileName}>Language</Text>
-      <Text style={styles.profileName}>Language List</Text>
-      <Text style={styles.profileName}>Privacy & Security</Text>
-      <Text style={styles.profileName}>Help & Support</Text>
-      <Text style={styles.profileName}>Send Feedback</Text>
+
+      <View style={styles.appearance}>
+        <Text style={styles.header}>Overall Progress</Text>
+        <Text style={styles.subHeader}>Task Completted</Text>
+        <View style={styles.appearanceSwtich}>
+          <Text style={styles.content}>80% completion rate</Text>
+          <Ionicons name="stats-chart" size={15} color="#fff" />
+        </View>
+      </View>
+
+      <View style={styles.appearance}>
+        <Text style={styles.header}>Appearance</Text>
+        <Text style={styles.subHeader}>Dark Mode</Text>
+        <View style={styles.appearanceSwtich}>
+          <Text style={styles.content}>Switch to dark theme</Text>
+          <Ionicons name="toggle" size={20} color="#fff" />
+        </View>
+      </View>
+
+      <View style={styles.appearance}>
+        <Text style={styles.header}>Preference</Text>
+        <Text style={styles.subHeader}>Language</Text>
+        <Text style={styles.content}>English</Text>
+      </View>
+      <View style={styles.appearance}>
+        <Text style={styles.subHeader}>Privacy & Security</Text>
+      </View>
+      <View style={styles.appearance}>
+        <Text style={styles.subHeader}>Help & Support</Text>
+      </View>
+      <View style={styles.appearance}>
+        <Text style={styles.subHeader}>Send Feedback</Text>
+      </View>
     </ThemedView>
   )
 }
@@ -31,15 +64,64 @@ const profile = () => {
 export default profile
 
 const styles = StyleSheet.create({
-    profileContainer: {
+  profileContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     padding: 16,
+    gap:10,
+    alignItems: "center",
+    marginTop: 30,
   },
   profileName: {
-    marginLeft: 12,
-    fontSize: 40,
+    fontSize: 30,
     fontWeight: '600',
     color: '#fff',
+    marginBottom: 10,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#fff',
+    marginBottom: 10,
+  },
+  subHeader: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#fff',
+  },
+  content: {
+    fontSize: 15,
+    fontWeight: '600',
+    color: '#fff',
+  },
+  appearance: {
+    borderWidth: 1,
+    borderColor: "#ffffff70",
+    borderRadius: 8,
+    margin: 10,
+    padding: 8,
+  },
+  appearanceSwtich: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  report: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: "space-around",
+    margin: 5,
+  },
+  reportCard: {
+    borderWidth: 1,
+    borderColor: '#ffffff70',
+    borderRadius: 12,
+    width: 180,
+    padding: 15,
+  },
+    title: {
+    fontSize: 18,
+    textAlign: "center",
+    fontWeight: 'bold',
+    color: 'white',
   },
 })
